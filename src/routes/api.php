2656f6ff07ae18f1e9cc224799d1a4ac603d7646
GauthierDefrance <defrance.gauthier@gmail.com>
2026-03-29 22:36:12 +0200

    Update api.php

--- src/routes/api.php
+++ src/routes/api.php
@@ -5,17 +5,15 @@ use App\Http\Controllers\CameraController;
 use App\Http\Controllers\CommandController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/camera/event', [CameraEventController::class, 'handle']);
-Route::get('/camera/events/{device}', [CameraEventController::class, 'list']);
+Route::prefix('api')->group(function () {
+    Route::post('/camera/event', [CameraEventController::class, 'handle']);
+    Route::get('/camera/events/{device}', [CameraEventController::class, 'list']);
 
-Route::post('/camera/heartbeat', [CameraController::class, 'heartbeat']);
-Route::post('/camera/status', [CameraController::class, 'cameraStatus']);
+    Route::post('/camera/heartbeat', [CameraController::class, 'heartbeat']);
+    Route::post('/camera/status', [CameraController::class, 'cameraStatus']);
 
-Route::get('/bridge/status', [CommandController::class, 'bridgeStatus']);
-
-Route::post('/api/test', function (\Illuminate\Http\Request $request) {
-    \Log::info('TEST API REÇU', $request->all());
-    return response()->json([
-        'received' => $request->all()
-    ]);
+    Route::get('/bridge/status', [CommandController::class, 'bridgeStatus']);
 });
+
+
+
